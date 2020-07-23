91a5d4db850ff8981d9a081c0cac3155692f3288
Yujin <yujin1679@naver.com>
2020-07-23 19:40:37 +0900

    5장 정리중

--- src/App.js
+++ src/App.js
@@ -1,61 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+  state = {
+    count: 0,
+  };
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  add = () => {   
+    this.setState({ count: 1 });
+  };
+
+  minus = () => {
+    this.setState({ count: -1 });
+  };
+
+  render() {
+    console.log("I'm rendering");
+    return (
       <div>
-        <h2>I like {name}</h2>
-        <h4>{rating}/5.0</h4>
-        <img src={picture} />
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
-  );
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://i.pinimg.com/originals/c4/c5/d5/c4c5d5d428634b5ddf1aabd9f98faf60.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://img.etoday.co.kr/pto_db/2019/07/600/20190726153503_1350707_1200_876.jpg',
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://post-phinf.pstatic.net/MjAyMDA0MTZfMjY3/MDAxNTg2OTk5MzYwOTMw.YZk3XJCkJqOrZmSzXTGfnXcfoj5CoLQfY9kEBhBmlyYg.WdEgjt1SmPLlCfi8nmVMB79FymTDi3ApEfQJrGF57Acg.JPEG/1.jpg?type=w1200',
-    rating: 4.0,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://www.nongsaro.go.kr/ps/img/interabang/num207/headerImg.jpg',
-    rating: 3.5,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} rating={dish.rating} />))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
-};
 
 export default App;
