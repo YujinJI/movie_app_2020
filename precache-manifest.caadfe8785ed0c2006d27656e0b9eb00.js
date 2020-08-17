c4f0242b9e71817db4d002ade4647f5368042a14
YujinJI <yujin1679@naver.com>
2020-08-17 18:16:50 +0900

    Updates

---
+++ precache-manifest.caadfe8785ed0c2006d27656e0b9eb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32268c67d2efb83981e825a9ae989ff",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "cac254a4d01b1d7d8c38",
+    "url": "/movie_app_2020/static/css/main.b9d1f250.chunk.css"
+  },
+  {
+    "revision": "102c6ec261a8945fdc0d",
+    "url": "/movie_app_2020/static/js/2.8021ebd8.chunk.js"
+  },
+  {
+    "revision": "eab6cd95c8b7f7a43af593f2585901b3",
+    "url": "/movie_app_2020/static/js/2.8021ebd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac254a4d01b1d7d8c38",
+    "url": "/movie_app_2020/static/js/main.c8e791bf.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
